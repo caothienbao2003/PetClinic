49126d852c354659ac723f8fb5a4bdd4fa6eb09c
caothienbao <baocaothien123@gmail.com>
2024-07-04 20:56:35 +0700

    [Add] Enum

---
+++ PetClinic_BussinessObject/Enum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClinicBussinessObject
+{
+    public enum ActiveStatus
+    {
+        UnActive,
+        Active
+    }
+
+    public enum UserRole
+    {
+        Customer,
+        Staff,
+        Doctor
+    }
+
+    public enum BookingStatus
+    {
+        Pending,
+        Canceled,
+        Missed,
+        Completed
+    }
+
+    public enum BookingPaymentStatus
+    {
+        Unpaid,
+        Paid
+    }
+
+    public enum ShiftType
+    {
+        Staff,
+        Doctor
+    }
+}
