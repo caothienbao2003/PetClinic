8f7e6bd0d9d62f9e820c93716e4d97dc56f3e1be
Kiengamedev <thorkien510@gmail.com>
2024-07-04 10:43:46 +0700

    Fixing db

--- PetClinic_BussinessObject/Booking.cs
+++ PetClinic_BussinessObject/Booking.cs
@@ -15,16 +15,16 @@ namespace PetClinicBussinessObject
         public int BookingId { get; set; }
         public int? PetId { get; set; }
         public int? DoctorId { get; set; }
-        public int? DoctorShiftId { get; set; }
+        public int? ShiftId { get; set; }
         public int? ServiceId { get; set; }
         public DateTime? BookingDate { get; set; }
         public string? PaymentStatus { get; set; }
         public string? Status { get; set; }
 
         public virtual User? Doctor { get; set; }
-        public virtual DoctorShift? DoctorShift { get; set; }
         public virtual Pet? Pet { get; set; }
         public virtual Service? Service { get; set; }
+        public virtual Shift? Shift { get; set; }
         public virtual ICollection<Feedback> Feedbacks { get; set; }
         public virtual ICollection<Invoice> Invoices { get; set; }
         public virtual ICollection<MedicalRecord> MedicalRecords { get; set; }
