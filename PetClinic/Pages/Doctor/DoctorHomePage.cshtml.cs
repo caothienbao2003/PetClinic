ba2cdc88b8d90d2a4f7d06e368eaa5fbd3243e5b
KeySurv <luonglkse171576@fpt.edu.vn>
2024-07-19 09:56:31 +0700

    Update DoctorHomePage

--- PetClinic/Pages/Doctor/DoctorHomePage.cshtml.cs
+++ PetClinic/Pages/Doctor/DoctorHomePage.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using PetClinicBussinessObject;
+using PetClinicServices.Interface;
 
 namespace PetClinic.Pages.Doctor
 {
     public class DoctorHomePageModel : PageModel
     {
+        private readonly IUserService userService;
+
+        public DoctorHomePageModel(IUserService _userService)
+        {
+            userService = _userService;
+        }
+
+        [BindProperty]
+        public User user { get; set; } = default!;
+
         public void OnGet()
         {
+            string userId = HttpContext.Session.GetString("UserId");
+            if (userId == null)
+            {
+                user = new User();
+            }
+            else
+            {
+                user = userService.GetUserById(int.Parse(userId));
+            }
         }
     }
 }
