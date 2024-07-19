3a859733638f10713fd17809e7569ba12f4cdfae
KSuvery <luonglkse171576@fpt.edu.vn>
2024-07-19 21:48:38 +0700

    Add Authorization [WIP]

--- PetClinic/Pages/Staff/CageManagement/Index.cshtml.cs
+++ PetClinic/Pages/Staff/CageManagement/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Humanizer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ using PetClinicServices.Interface;
 
 namespace PetClinic.Pages.Staff.CageManagement
 {
+    //[Authorize(Roles = "Staff")]
     public class IndexModel : PageModel
     {
         private readonly ICageService cageService;
@@ -26,6 +28,26 @@ namespace PetClinic.Pages.Staff.CageManagement
         public void OnGet()
         {
             Cage = cageService.GetAllCage();
+            string role = HttpContext.Session.GetString("Role");
+            if (role == null)
+            {
+                Response.Redirect("/Authentication/Login");
+            }
+            else if(role != "1")
+            {
+                if (role == "0")
+                {
+                    Response.Redirect("/Customer/CustomerHomePage");
+                }
+                else if (role == "2")
+                {
+                    Response.Redirect("/Doctor/DoctorHomePage");
+                }
+                else
+                {
+                    Response.Redirect("/Admin/AdminHomePage");
+                }
+            }
         }
     }
 }
