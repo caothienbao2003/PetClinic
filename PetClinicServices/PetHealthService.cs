427ac2722d750a6d0123abb0d998285fa8bcb356
KSuvery <luonglkse171576@fpt.edu.vn>
2024-07-22 05:38:01 +0700

    Add PetHealth Layers; Update PetManagement Create for Customer

---
+++ PetClinicServices/PetHealthService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetClinicServices.Interface;
+using PetClinicBussinessObject;
+using PetClinicRepository;
+using PetClinicRepository.Interface;
+
+namespace PetClinicServices
+{
+    public class PetHealthService : IPetHealthService
+    {
+        private readonly IPetHealthRepository petHealthRepository;
+
+        public PetHealthService()
+        {
+            if (petHealthRepository == null)
+            {
+                petHealthRepository = new PetHealthRepository();
+            }
+        }
+
+        public List<PetHealth> GetAllPetHealths()
+        {
+            return petHealthRepository.GetAllPetHealths();
+        }
+
+        public PetHealth GetPetHealthByPetId(int petId)
+        {
+            return petHealthRepository.GetPetHealthByPetId(petId);
+        }
+
+        public PetHealth GetPetHealthById(int petHealthId)
+        {
+            return petHealthRepository.GetPetHealthById(petHealthId);
+        }
+
+        public void AddPetHealth(PetHealth petHealth)
+        {
+            petHealthRepository.AddPetHealth(petHealth);
+        }
+
+        public void UpdatePetHealth(PetHealth petHealth)
+        {
+            petHealthRepository.UpdatePetHealth(petHealth);
+        }
+    }
+}
---
+++ PetClinic_Repository/PetHealthRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetClinicBussinessObject;
+using PetClinicRepository.Interface;
+using PetClinicDAO;
+
+namespace PetClinicRepository
+{
+    public class PetHealthRepository : IPetHealthRepository
+    {
+        public List<PetHealth> GetAllPetHealths() => PetHealthDAO.Instance.GetAllPetHealths();
+
+        public PetHealth GetPetHealthByPetId(int petId) => PetHealthDAO.Instance.GetPetHealthByPetId(petId);
+
+        public PetHealth GetPetHealthById(int petHealthId) => PetHealthDAO.Instance.GetPetHealthById(petHealthId);
+
+        public void AddPetHealth(PetHealth petHealth) => PetHealthDAO.Instance.AddPetHealth(petHealth);
+
+        public void UpdatePetHealth(PetHealth petHealth) => PetHealthDAO.Instance.UpdatePetHealth(petHealth);
+    }
+}
