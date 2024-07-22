c5de5a5e524edd4e8a7bf5424342531d94c82fc3
HoThanhKhoi <th.khoi2003@gmail.com>
2024-07-22 07:02:03 +0700

    Admin View Customers

--- PetClinic/Pages/Admin/AdminManageCustomer/AdminViewCustomerList.cshtml.cs
+++ PetClinic/Pages/Admin/AdminManageCustomer/AdminViewCustomerList.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetClinicBussinessObject;
+using PetClinicServices.Interface;
 
 namespace PetClinic.Pages.Admin.AdminManageCustomer
 {
     public class AdminViewCustomerListModel : PageModel
     {
-        public void OnGet()
+		[BindProperty]
+		public decimal revenue { get; set; }
+		[BindProperty]
+		public int totalCustomers { get; set; }
+		[BindProperty]
+		public int totalBookings { get; set; }
+
+		[BindProperty]
+		public List<User> Customers { get; set; }
+
+		private readonly IUserService userService;
+
+		public AdminViewCustomerListModel(IUserService _userService)
+		{
+			userService = _userService;
+		}
+		public void OnGet()
         {
-        }
+			Customers = userService.GetAllUsers();
+		}
+
+
     }
 }
