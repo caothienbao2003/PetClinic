8ff75615b0247be02de3bfa4c90260e6664ee162
KSuvery <luonglkse171576@fpt.edu.vn>
2024-07-25 00:43:48 +0700

    Fix Edit Pet

--- PetClinic/Pages/Customer/PetManagement/Edit.cshtml.cs
+++ PetClinic/Pages/Customer/PetManagement/Edit.cshtml.cs
@@ -7,35 +7,61 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PetClinicBussinessObject;
+using PetClinicServices.Interface;
 
 namespace PetClinic.Pages.Customer.PetManagement
 {
     public class EditModel : PageModel
     {
-        private readonly PetClinicBussinessObject.PetClinicContext _context;
+        private readonly IPetService petService;
+        private readonly IUserService userService;
+        private readonly IPetHealthService petHealthService;
 
-        public EditModel(PetClinicBussinessObject.PetClinicContext context)
+        public int userId = 0;
+        public string? userIdString;
+
+        public EditModel(IPetService _petService, IUserService _userService
+        , IPetHealthService _petHealthService)
         {
-            _context = context;
+            petService = _petService;
+            userService = _userService;
+            petHealthService = _petHealthService;
         }
 
         [BindProperty]
         public Pet Pet { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null || _context.Pets == null)
-            {
-                return NotFound();
-            }
+        [BindProperty(SupportsGet = true)]
+        public int PetId { get; set; }
 
-            var pet =  await _context.Pets.FirstOrDefaultAsync(m => m.PetId == id);
+        [BindProperty]
+        public PetHealth PetHealth { get; set; } = default!;
+
+        [BindProperty]
+        public User user { get; set; } = default!;
+
+        public IActionResult OnGet(int id)
+        {
+            PetId = id;
+
+            var pet = petService.GetPetById(id);
+
             if (pet == null)
             {
                 return NotFound();
             }
+
             Pet = pet;
-           ViewData["CustomerId"] = new SelectList(_context.Users, "UserId", "Password");
+
+            var petHealth = petService.GetPetHealthByPetId(PetId);
+
+            if (petHealth == null)
+            {
+                return NotFound();
+            }
+
+            PetHealth = petHealth;
+
             return Page();
         }
 
@@ -43,35 +69,46 @@ namespace PetClinic.Pages.Customer.PetManagement
         // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            ModelState.Clear();
             if (!ModelState.IsValid)
             {
+                ModelState.AddModelError(string.Empty, "ModelState is NOT valid");
                 return Page();
             }
 
-            _context.Attach(Pet).State = EntityState.Modified;
+            Pet existingPet = petService.GetPetById(PetId);
+            PetHealth existingPetHealth = petHealthService.GetPetHealthByPetId(PetId);
+            if (existingPet == null)
+            {
+                ModelState.AddModelError(string.Empty, "Error occurred while updating pet.");
+                return Page();
+            }
+
+            if (existingPetHealth == null)
+            {
+                ModelState.AddModelError(string.Empty, "Error occurred while updating pet health.");
+                return Page();
+            }
 
             try
             {
-                await _context.SaveChangesAsync();
+                existingPet.PetName = Pet.PetName;
+                existingPet.PetType = Pet.PetType;
+                existingPet.PetAge = Pet.PetAge;
+                existingPetHealth.OverallHealth = PetHealth.OverallHealth;
+                existingPetHealth.Conditions = PetHealth.Conditions;
+                existingPetHealth.Weight = PetHealth.Weight;
+                existingPetHealth.WeightMeasurementDate = PetHealth.WeightMeasurementDate;
+
+                petService.UpdatePet(existingPet);
+                petHealthService.UpdatePetHealth(existingPetHealth);
+                return RedirectToPage("/Customer/PetManagement/Index", new {id = PetId});
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!PetExists(Pet.PetId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                ModelState.AddModelError(string.Empty, "Error occurred while updating pet.");
+                return Page();
             }
-
-            return RedirectToPage("./Index");
-        }
-
-        private bool PetExists(int id)
-        {
-          return (_context.Pets?.Any(e => e.PetId == id)).GetValueOrDefault();
         }
     }
 }
